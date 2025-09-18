2100ec9d02df5c22da904d8aaaf7ca04350d1d8b
rodsilca <cabralrodrigo787@gmail.com>
2025-09-18 18:54:21 -0400

    backend

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users.js");
+const authRoutes = require("./routes/auth.js");
+
+connection();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, ()=>{
+    console.log("started on: "+ PORT);
+})
