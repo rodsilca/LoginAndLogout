6f169bf9a5b40f2a4cc595a9dfce3a8f0bab11ee
rodsilca <cabralrodrigo787@gmail.com>
2025-09-18 18:58:17 -0400

    frontend

---
+++ client/src/components/Main/index.jsx
@@ -0,0 +1,23 @@
+import styles from "./styles.modules.css";
+
+const Main = ()=>{
+    
+    const handleLogout = () =>{
+        localStorage.removeItem("token");
+        window.location.reload();
+    }
+    
+    return (
+        <div className={styles.main_container}>
+            <nav className={styles.navbar}>
+                <h1>dfskgjl</h1>
+                <button className={styles.white_btn} onClick={handleLogout}>
+                    Logout
+                </button>
+            </nav>
+        </div>
+        
+    )
+}
+
+export default Main;
